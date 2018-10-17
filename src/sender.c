c110208b7d28a5fde63bf23225283857f4eed762
constdesch <constantin.deschaetzen@student.uclouvain.be>
2018-10-17 10:36:45 +0200

    Import fonctions inginious + modifications sender.c

--- src/sender.c
+++ src/sender.c
@@ -2,7 +2,7 @@
 //  sender.c
 //  
 //
-//  Created by Constantin de Schaetzen on 16/10/18.
+//  Created by Constantin de Schaetzen and Jean Gillain on 16/10/18.
 //
 
 #include <string.h>
@@ -75,45 +75,56 @@ void sender(int argc, char* argv[]){
     /*Create a socket bound and connected */
     int sfd = create_socket(&src_addr, port, &dst_addr, port); /* Connected */ /* src_port = dst_port ? */
     if(sfd==-1) fprintf(stderr,"Could not create socket.\n");
+    
     /* Option -f mentionned */
     if(filename != NULL){
+        file = fopen(filename, "r");/* Only for reading */
+    }
+    else{ /* Option f not mentionned */
+        filename = argv[argc-1] /*ATTENTION suggere que ce sera toujours le dernier element */
         file = fopen(filename, "r"); /* Only for reading */
-        if(file!=NULL) {
-            char buf[512];
-            int byteRead = read(file,buf,512);
-            if (byteRead==-1) fprintf(stderr,"Could not read in file specified\n");
-            while(byteRead>0){
-                pkt_t* pkt = pkt_new();
-                pkt_set_type(pkt,PTYPE_DATA);
-                pkt_set_tr(pkt,0);
-                pkt_set_window();
-                pkt_set_seqnum(pkt,seqnum);
-                seqnum++;
-                pkt_set_length(pkt,byteRead);
-                pkt_set_payload(pkt,buf,byteRead);
-                uLong crc2= crc32(0L, Z_NULL, 0);
-                crc2= crc32(crc2, (Bytef *) buf+12,pkt->length);
-                uLong crc1 = crc32(0L, Z_NULL, 0);
-                pkt_set_crc1(pkt,htonl(crc1));
-                pkt_set_crc2(pkt,htonl(crc2));
-                data_length = byteRead +16;
-                char *data = malloc(data_length);
-                if(!data) fprintf(stderr,"Data malloc failed\n");
-                status = pkt_encode(pkt,data,data_length);
-                if(status!=PKT_OK) fprintf(stderr,"Encode failed : %d\n",status);
-                err = write(sfd,data,data_length);
-                if(err==-1) fprintf(stderr,"Could not write on the socket.\n");
-                buf = memset(buf,0,sizeof(buf));
-                byteRead = read(file,buf,512)
-            }
-        
+    }
+    if(file!=NULL) {
+        char buf[512];
+        int byteRead = read(file,buf,512);
+        if (byteRead==-1) fprintf(stderr,"Could not read in file specified\n");
+        while(byteRead>0){
+            /* Create the header */
+            pkt_t* pkt = pkt_new();
+            pkt_set_type(pkt,PTYPE_DATA);
+            pkt_set_tr(pkt,0);
+            pkt_set_window();
+            pkt_set_seqnum(pkt,seqnum);
+            seqnum++;
+            pkt_set_length(pkt,byteRead);
+            pkt_set_payload(pkt,buf,byteRead);
+            uLong crc2= crc32(0L, Z_NULL, 0);
+            crc2= crc32(crc2, (Bytef *) buf+12,pkt->length);
+            uLong crc1 = crc32(0L, Z_NULL, 0);
+            pkt_set_crc1(pkt,htonl(crc1));
+            pkt_set_crc2(pkt,htonl(crc2));
             
+            /* Encode the header */
+            data_length = byteRead +16;
+            char *data = malloc(data_length);
+            if(!data) fprintf(stderr,"Data malloc failed\n");
+            status = pkt_encode(pkt,data,data_length);
+            if(status!=PKT_OK) fprintf(stderr,"Encode failed : %d\n",status);
+            
+            err = write(sfd,data,data_length);
+            if(err==-1) fprintf(stderr,"Could not write on the socket.\n");
+            
+            buf = memset(buf,0,sizeof(buf));
+            byteRead = read(file,buf,512)
+        }
     }
     
+        
+        
     
 
         
-    }*/
+    }
     
 }
 
