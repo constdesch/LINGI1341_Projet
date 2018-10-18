e2c3bca939d70a5d9c69c444e357bfdb791b0ef1
Jean Gillain <jegillain@calrissian.info.ucl.ac.be>
2018-10-18 11:44:31 +0200

    changement dans la queue plus circulaire

--- src/tools/queue_pkt.c
+++ src/tools/queue_pkt.c
@@ -22,83 +22,76 @@ void free_Node(Node * node){
     free(node);
   }
 }
-Node * addHead(queue_pkt *list, pkt_t *data) {
+Node * addTail(queue_pkt *list, pkt_t *data) {
   Node *node= (Node *) malloc(sizeof(Node));
   if(node==NULL)
   return NULL;
   node->data=data;
   if(list->head==NULL) {
-    list->tail=node;
+    list->head=node;
     node->next=NULL;
+    list->tail=NULL;
+    list->full++;
+    return node;
   } else {
-    node->next=list->head;
-  }
-  list->head=node;
+  Node *node1=list->tail;
+  node1->next=node;
+  node->next=NULL;
+  list->full++;
+  list->tail=node;
   list->full++;
   return node;
 }
-int deleteAllYounger(queue_pkt *list, int age) {
+int deletePrevious(queue_pkt *list, int seqnum) {
   if(list==NULL)
   {return -1;}
   if(list->head==NULL){
     return -1;
   }
   Node *node1=list->head;
-  pkt_t * pkt=node1->data;
   int count=0;
-  while(pkt->window<age){
+  while(count!=seqnum){
     Node *node3=node1;
     if(node1->next!=NULL){
       node1=node1->next;
-      empl=node1->data;
-      free(node3);
+      free_Node(node3);
       list->full--;
       count++;
-      list->head=node1;
+      list->head==node1;
     }
     else{
-      node1=node1->next;
-      free(node3);
-      list->head=node1;
-      list->tail=node1;
-      count++;
+      free_Node(node3);
       list->full--;
+      count++;
+      list->head=NULL;
+      list->tail==NULL;
       return count;
     }
   }
-  if(node1->next==NULL){
-    list->tail=node1;
-    list->head=node1;
-    return count;
-  }
-  Node *node2=node1->next;
-  pkt_t *pkt1=node2->data;
-  while(node2!=NULL){
-    empl1=node2->data;
-    if(pkt1->window<age){
-      if(node2!=list->tail){
-        Node *node3=node2;
-        node1->next=node2->next;
-        node2=node2->next;
-        free(node3);
-        count++;
-        list->full--;
-      }
-      else{
-        Node *node3=node2;
-        node1->next=node2->next;
-        node2=node2->next;
-        free(node3);
-        count++;
-        list->full--;
-        list->tail=node1;
-        return count;
-      }
-    }
-    else{
-      node1=node1->next;
-      node2=node2->next;
-    }
-  }
   return count;
+}
+pkt_t * getPos(queue_pkt * list, int position){
+  if(list==NULL){
+    fprintf(stderr,"la liste n'existe pas\n");
+    return NULL;
+  }
+  if(list->head==NULL){
+    fprintf(stder,"la liste est vide \n");
+    return NULL;
+  }
+  else{
+    int pos=0;
+    Node * node1=list->head;
+    while(pos!=position && node1!=NULL){
+      node1=node1->next;
+      pos++;
+    }
+    if(node1==NULL){
+      fprintf(stderr,"il n'y a pas autant de positions dans la liste \n");
+      return NULL;
+    }
+    pkt_t* pkt=node1->data;
+    return pkt;
+    }
+  }
 }
